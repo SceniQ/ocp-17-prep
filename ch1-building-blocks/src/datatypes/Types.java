cf75ca377b4aa44f0ff3e98c94cccab6c328bb95
SceniQ <tassagaai19@gmail.com>
2024-06-08 15:15:22 +0200

    datatypes exmaples

---
+++ ch1-building-blocks/src/datatypes/Types.java
@@ -0,0 +1,14 @@
+package datatypes;
+
+/*
+	* Java applications contain two types of data: primitive types and reference types
+*/
+
+public class Types{
+	
+	// DOES NOT COMPILE - Java complains the number is out of range. And it is—for
+	// an int. However, we don’t have an int. The solution is to add the character L to the number:
+	//long max = 3123456789; 
+	
+	long max = 3123456789L; // Now Java knows it is a long
+}
--- ch1-building-blocks/src/objects/Chicken.java
+++ ch1-building-blocks/src/objects/Chicken.java
@@ -2,6 +2,8 @@ package objects;
 
 public class Chicken {
 	
+	//{ System.out.println(name); } // DOES NOT COMPILE - illegal forward reference
+	
 	private String name = "Fluffy";
 	{ System.out.println("setting field"); }
 	
@@ -12,7 +14,31 @@ public class Chicken {
 	
 	public static void main(String[] args) {
 		Chicken chicken = new Chicken();
-		System.out.println(chicken.name); 
-	} 
+		System.out.println(chicken.name);
+		String test = "test1";
+		testingString(test);
+		
+	}
+
+	private static void testString(String testingString){
+		testingString = "test2";
+		System.out.println(testingString);
+	}
  
  }
+
+// Order of execution 
+class Egg {
+	public Egg() {
+		number = 5;
+	}
+
+	public static void main(String[] args) {
+		Egg egg = new Egg();
+		System.out.println(egg.number);
+	}
+	
+	private int number = 3;
+	{ number = 4; } 
+	
+}
