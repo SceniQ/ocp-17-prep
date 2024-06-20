d7dc1e9f62a1759ff0537bb841895b5318277786
SceniQ <tassagaai19@gmail.com>
2024-06-20 23:09:01 +0200

    operator precendence example

---
+++ ch2-operators/src/Operator.java
@@ -0,0 +1,44 @@
+public class Operator{
+	public static void main(String... args){
+		int cookies = 4;
+		double reward = 3 + 2 * --cookies;
+		System.out.print("Zoo animal receives: "+((int)reward >> 2) +" reward points");
+	}
+}
+
+class Camel {
+ 
+  { int hairs = 3_000_0; }
+ 
+	long water, air=2;
+	boolean twoHumps = true;
+ 
+	public void spit(float distance) {
+		var path = "";
+		
+		{ double teeth = 32 + distance++; }
+		
+			while(water > 0) {
+				int age = twoHumps ? 1 : 2;
+				short i=-1;
+			
+				for(i=0; i<10; i++) {
+					var Private = 2;
+				}
+			// SCOPE: 7 - water, air, twoHumps, distance, path, age, i
+			}
+	}
+}
+
+class KitchenSink {
+	
+	private int numForks;
+
+	public static void main(String[] args) {
+		int numKnives;
+		System.out.print("""
+			"# forks = " + numForks +
+			" # knives = " + numKnives +
+			# cups = 0""");
+	}
+}
